0aaf87cd7265913b87f397fc577bf4a87116345f
Sulthonhanan <hananakhnaf31@gmail.com>
2025-06-10 10:20:18 +0700

    Menambahkan blok catch khusus tipe int

--- Manual Exception.cpp
+++ Manual Exception.cpp
@@ -5,8 +5,12 @@ int main()
 {
     try {
         cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 0.5; // melemparkan sebuah nilai bertipe double
+    }
+        catch (int a) {
+        //blok ini akan dieksekusi jika throw melempar integer
+        cout << "Pengecualia akan dieksekusi" << endl;
     }
 
-    throw 0.5; // melemparkan sebuah nilai bertipe double
 
 }
